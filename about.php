2cbc2492e2a9ae65ce414d53e37fdb3642a2b77c
unknown <waleed633@icloud.com>
2017-10-20 13:18:28 +0500

    about page complete

---
+++ about.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php require './navigations/header.php' ?>  
+  </head>
+  
+  <body class="about-body">
+    <?php include './navigations/navigation.php' ?>
+
+    <section class="about-main">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="container">
+                    <div class="col-xl-12">
+                        <div class="parent">
+                            <h1 class="about-heading">Our<br> Story</h1>
+                            <p class="para">
+                                We started The Jacket Maker to simplify personalization in fashion indistry. We combine years-old bespoke service with today’s technology to make finest quality custom and ready-to-wear clothing accessible to everyone
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="parent2 text-right">
+                        <h1 class="about-heading2">“we  believe you should  never settle for anything almost  perfect”</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="second mt-5">
+        <div class="container px-5">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="second-div">
+                        <p class="second-p">Based out of Dubai, UAE & Karachi, Pakistan - We are a group of young, exciting individuals looking to disrupt outerwear industry. We are changing the rules of fashion retail. Our new rules empower customers and give them control over what they choose to wear. We started the bespoke service keeping in mind simple solutions to three problems.</p>
+                        <h1 class="second-head">Accessibility</h1>
+                        <p class="second-p">Bespoke has always existed but contrary to fast-fashion, it has never been accessible to all. Our custom leather jackets starting at $350 being as affordable as mass-produced leather jackets is the best solution to bespoke accessibility</p>
+                        <h1 class="second-head">Convenience</h1>
+                        <p class="second-p">We wanted to make bespoke process as easy and convenient as possible. With The Jacket Maker you get a dedicated design consultant to work on your custom project without any charges and final commitment to purchase.</p>
+                        <h1 class="second-head">Perfection</h1>
+                        <p class="second-p">How often do you find a product which you like but there is something you want to change about it. We believe you should never settle for anything almost perfect.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="third mt-5">
+        <div class="container-fluid">
+            <div class="container px-5">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <h1 class="third-head">The<br>Journey</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="fourth mt-5">
+        <div class="container-fluid">
+            <div class="container px-5">
+                <div class="row">
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+                        <h1 class="fourth-head">2013</h1>
+                        <div class="break hidden-sm-down"></div>
+                    </div>
+            
+                    <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10">
+                        <p class="fourth-p">In 2013, we started as an online retail store for leather jackets, with a small division for custom orders. Our retail store majorly targeted comic and movie fans with finest quality leather jackets. Since day one, we didn’t believe in the middlemen theory, and didn’t like the way the fashion industry functioned, with a plethora of middlemen in between the manufacturer and customer. We initiated by being the manufacturer and the retailer of our products, a humble customer-centric brand, making top notch jackets.</p>
+                    </div>
+                </div>
+
+                <div class="row mt-5">
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+                        <h1 class="fourth-head">2016</h1>
+                        <div class="break2 hidden-sm-down"></div>
+                    </div>
+            
+                    <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10">
+                        <p class="fourth-p">In 2016, we shifted our complete focus to bespoke with a simple idea, "Make it easy for people to buy leather jackets of their choice". From there on we went on to adding one outerwear category at a time, and we are not done yet! </p>
+                        <p class="fourth-p">It was this achievement of discovering a need to empower people in their right to choose what apparel they would like, which ignited a spark that led to the transformation of The Jacket Maker. The small division for custom order that we had, taught us many things. The most amazing discovery was "There is something amazing about wearing a jacket that is specially made for you & Just for you". </p>
+                        <p class="fourth-p">For our ready-to-wear collection, we continued with our same old belief that it starts with the product and ends with the product, everything else is followed. With our design community contributing to ready to wear collection, we aim to offer the widest range of finest quality leather jackets at an affordable price</p>
+                    </div>
+                </div>
+
+                <div class="row mt-5">
+                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2">
+                        <h1 class="fourth-head">2017</h1>
+                        <!-- <div class="break hidden-sm-down"></div> -->
+                    </div>
+            
+                    <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10">
+                        <p class="fourth-p">In 2017, with the same focus of making custom easy and accessible, we begun focusing on providing the best digital experience for bespoke. We built a team of in-house design consultants to offer a personalized experience from start to scratch. We made developments on our website to offer improved digital experience and we are continuously hungry looking for solution to improve our bespoke experience.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="fifth mt-5">
+        <div class="container-fluid">
+            <div class="container px-5">
+                <h1 class="fifth-head text-center pt-4 pb-5">Mission</h1>
+                <div class="row pb-5">
+                    <div class="col-xl-4 col-lg-4 col-md-4 offset-md-1 col-sm-12 text-center">
+                        <h1 class="fifth-h">Bespoke</h1>
+                        <p class="fifth-p">We want to provide the best personalized digital experience in the world for bespoke, from both service and product standpoint, and make it accessible for all</p>
+                    </div>
+
+                    <!-- <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 text-center">
+                    </div>                     -->
+
+                    <div class="col-xl-4 col-lg-4 col-md-4 offset-md-2 col-sm-12 text-center">
+                        <h1 class="fifth-h">Ready to Wear</h1>
+                        <p class="fifth-p">We want to offer the widest range of finest quality, authentic outerwear at affordable pricing under one roof with the help of our design community</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php require './navigations/footer.php' ?>  
+  </body>
+</html>
