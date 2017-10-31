a3cf5c889651eee6f9caec1a334111e561a62bb6
unknown <waleed633@icloud.com>
2017-10-31 19:39:10 +0500

    jacket dashboard remaining

--- dash.php
+++ dash.php
@@ -28,6 +28,25 @@
                <div class="col-xl-9 col-lg-9 col-md-8 col-sm-12 col-12">
                     <h6 class="dash-heading">Account Setting Panel</h6>
                     <hr>
+                    <div class="panel-box">
+                        <div class="name-box">
+                            <h6 class="name-echo">Hello Waleed Khan</h6>
+                            <p>From your My Account Dashboard you have the ability to view a snapshot of your recent account activity and update your account information. Select link below to view or edit information.</p>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                <div class="detail-box">
+                                    <h6 class="cntct-detail-head">Contact details</h6>
+                                    <hr>
+                                    <h6 class="cntct-name">Waleed Khan</h6>
+                                    <h6 class="cntct-name">WaleedKhan633@gmail.com - <a href="">Change E-mail</a> </h6>
+                                    <a href="#" class="pswrd-chnge">Change Password</a>
+                                    <h6 class="text-right mt-4"><a href="#" class="edit-anchor"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Edit</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                </div>
            </div>
        </div>
